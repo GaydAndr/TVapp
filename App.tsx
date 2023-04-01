0212e1029b77d27d920541600bad4fa97f2b9a56
andrii_h <galgayand@gmail.com>
2023-04-01 21:05:03 +0300

    update

--- App.tsx
+++ App.tsx
@@ -19,7 +19,7 @@ import {
   Button,
   TouchableOpacity
 } from 'react-native';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef  } from 'react';
 import {
   Colors,
   DebugInstructions,
@@ -29,6 +29,8 @@ import {
 } from 'react-native/Libraries/NewAppScreen';
 
 import { WebView } from 'react-native-webview';
+import Video from 'react-native-video';
+import shaka from 'shaka-player';
 
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -37,6 +39,7 @@ function App(): JSX.Element {
     height: Dimensions.get('window').height,
   });
   const [url, setUrl] = useState('');
+  const webViewRef = React.useRef<WebView>(null);
 
   useEffect(() => {
     const subscription = ({ window }) => {
@@ -48,6 +51,17 @@ function App(): JSX.Element {
     return () => windowHendler.remove();
   }, []);
 
+  const onWebViewMessage = (event: WebViewMessageEvent) => {
+     if (event.nativeEvent.data === 'loadPlayer') {
+       const player = new shaka.Player(webViewRef.current);
+       player.load('https://example.com/hls/stream.m3u8');
+     }
+  };
+
+  const onLoad = () => {
+    webViewRef.current.postMessage('loadPlayer');
+  };
+
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
@@ -55,22 +69,25 @@ function App(): JSX.Element {
   return (
     <View style={backgroundStyle}>
       <StatusBar
+      hidden={ dimensions.height < dimensions.width? true: false}
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
 
-      <View style={{ height: dimensions.height- 20, width: dimensions.width }}>
+      <View style={{ height: dimensions.height, width: dimensions.width }}>
         <WebView
+          allowsInlineMediaPlayback={true}
+          ref={webViewRef}
           source={{
             uri: 'http://tv.smartup.tv',
             headers: {
               'User-Agent': 'sandroid'
             }
           }}
-
+            onMessage={onWebViewMessage}
+                  onLoad={onLoad}
           style={{ width: dimensions.width}}
         />
-      
       </View>
     </View>
   );
